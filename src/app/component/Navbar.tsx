08b44149ec505f98d3ab6e39eeb048511e9e2257
Muhammad-Ali-Asghar-Rizwan <164878048+Muhammad-Ali-Asghar-Rizwan@users.noreply.github.com>
2024-11-21 12:47:43 +0500

    first commit

---
+++ src/app/component/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const NavbarPage = () => {
+  return (
+    <div>
+      <div className='container'>
+        <div className='logo'>Ai Services</div>
+        <ul>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default NavbarPage
